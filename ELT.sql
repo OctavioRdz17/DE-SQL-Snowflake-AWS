d76e4491b6656a54c9e4c6964b685cb8d9a0505d
OctavioRdz17 <octavio.rdzrojas@gmail.com>
2023-09-24 10:14:24 -0600

    update sync kim

--- ELT.sql
+++ ELT.sql
@@ -10,23 +10,584 @@ CREATE OR REPLACE TABLE TablaCalendario AS
     DATE_PART(DAY, DATEADD(DAY, seq4(), '2015-01-01')) AS DiaMes
   FROM TABLE(GENERATOR(ROWCOUNT => 3000));
 
+
+-- Check all tables one by one
+
+---------- TABLE CANAL_VENTA ------------
+
+SELECT * FROM canal_venta; -- looks good
+
+
+----------- TABLE CLIENTE -----------------
+
+SELECT * FROM cliente; 
+DESCRIBE TABLE cliente;
+
+-- Provincia y Localidad, standardize names 
+
+SELECT DISTINCT Provincia -- Looks good
+FROM cliente;
+
+SELECT DISTINCT Localidad -- Looks good
+FROM cliente
+ORDER BY 1;
+
+-- Update the "NOMBREAPELLIDO" column to capitalize its contents. -- 3407 rows affected
+UPDATE cliente
+SET NombreApellido = INITCAP(NombreApellido);
+
+-- Capitalize Direccion -- 3407 rows affected
+UPDATE cliente
+SET direccion = INITCAP(direccion);
+
+--Capitalize LOCALIDAD --3407 rows affected
+UPDATE cliente
+    SET localidad = INITCAP(localidad);
+
+-- Categorize Edad
+-- Create a new column "AgeCategory" based on the "EDAD" column.
+
+ALTER TABLE cliente
+ADD CategoriaEdad VARCHAR(50);
+
+UPDATE cliente -- 3407 rows affected
+SET CategoriaEdad =
+  CASE
+    WHEN edad < 18 THEN 'Menor de 18'
+    WHEN edad BETWEEN 19 AND 29 THEN 'Entre 19 y 29'
+    WHEN edad BETWEEN 30 AND 39 THEN 'Entre 30 y 39'
+    WHEN edad BETWEEN 40 AND 49 THEN 'Entre 40 y 49'
+    WHEN edad BETWEEN 50 AND 59 THEN 'Entre 50 y 59'
+    ELSE 'Mayor de 60'
+  END;
+
+SELECT DISTINCT Localidad
+FROM cliente
+WHERE Localidad LIKE '%Buenos%';
+
+UPDATE cliente -- 458 rows updated
+SET localidad = 'Ciudad de Buenos Aires'
+WHERE localidad = 'Ciudad De Buenos Aires';    
+
+-- Check format for LONGITUD y LATITUD, turn into decimal
+-- Convert columns to decimal
+UPDATE cliente -- 3407 rows affected
+SET
+    longitud = CAST(REPLACE(longitud, ',', '.') AS DECIMAL(18,2)),
+    latitud = CAST(REPLACE(latitud, ',', '.') AS DECIMAL(18,2));
+
+-- Remove column COL10
+ALTER TABLE cliente
+DROP COLUMN col10;
+
+-- Check for duplicates in id
+SELECT IdCliente, COUNT(*)
+FROM cliente
+GROUP BY IdCliente
+HAVING COUNT(*) > 1; -- no duplicates
+
+-- Fill missing values in categorical columns
+UPDATE cliente -- 31 rows updated
+SET provincia = COALESCE(provincia, 'Sin Dato')
+WHERE provincia IS NULL;
+
+UPDATE cliente -- 46 rows updated
+SET NombreApellido  = COALESCE(NombreApellido, 'Sin Dato')
+WHERE NombreApellido IS NULL;
+
+UPDATE cliente -- 48 rows updated
+SET direccion = COALESCE(direccion, 'Sin Dato')
+WHERE direccion IS NULL;
+
+UPDATE cliente -- 32 rows updated
+SET localidad = COALESCE(localidad, 'Sin Dato')
+WHERE localidad IS NULL;
+
+
+----------- TABLE SUCURSAL -- -----------
+
+SELECT * FROM sucursal; 
+
+-- Check SUCURSAL names
+SELECT DISTINCT sucursal
+FROM sucursal
+ORDER BY sucursal; 
+
+-- Change Palermo 1 for Palermo1 and MDQ1 and MDQ2 for Mar del Plata1 and Mar del Plata2
+
+UPDATE sucursal -- 31 rows affected
+SET sucursal =
+    CASE
+        WHEN sucursal = 'Palermo 1' THEN 'Palermo1'
+        WHEN sucursal = 'Palermo 2' THEN 'Palermo2'
+        WHEN sucursal = 'MDQ1' THEN 'Mar del Plata1'
+        WHEN sucursal = 'MDQ2' THEN 'Mar del Plata2'
+        ELSE sucursal
+    END;
+        
+-- Standardize LOCALIDAD y PROVINCIA
+SELECT DISTINCT localidad
+FROM sucursal
+ORDER BY localidad;
+
+UPDATE sucursal -- 8 rows affected
+SET localidad = 'Ciudad de Buenos Aires'
+WHERE TRIM(localidad) IN ('CABA', 'Cap. Federal','Cap.   Federal', 'Cap. Fed.', 'CapFed', 'Capital',
+                    'Capital Federal', 'Cdad de Buenos Aires', 'Ciudad De Buenos Aires');
+
+UPDATE sucursal  -- 2 rows affected
+SET localidad = 'Córdoba'
+WHERE TRIM(localidad) IN ('Coroba', 'Cordoba');
+
+SELECT DISTINCT provincia
+FROM sucursal
+ORDER BY provincia;
+
+UPDATE sucursal -- 8 rows updated
+SET provincia = 'Buenos Aires'
+WHERE TRIM(provincia) IN ('B. Aires', 'B.Aires', 'Bs As', 'Bs.As.', 'Pcia Bs AS', 
+                    'Prov de Bs As.', 'Provincia de Buenos Aires');
+UPDATE sucursal  -- 4 rows updates
+SET provincia = 'CABA'
+WHERE TRIM(provincia) IN ('C deBuenos Aires', 'Ciudad de Buenos Aires', 'Ciudad De Buenos Aires');
+
+UPDATE sucursal  -- 1 row updated
+SET provincia = 'Córdoba'
+WHERE TRIM(provincia) = 'Cordoba';
+
+
+-- Check LATITUD y LONGITUD, change positions and turn into decimal
+UPDATE sucursal -- 31 rows updated
+    SET latitud = CAST(REPLACE(latitud, ',', '.') AS DECIMAL(18,2)),
+        longitud = CAST(REPLACE(longitud, ',', '.') AS DECIMAL(18,2));
+
+-- Change positions, first longitud and the latitud. 
+-- Because there are no REORDER command, I create a new column that it's added at the end
+ALTER TABLE sucursal
+ADD latitud_ DECIMAL(18,2);
+
+UPDATE sucursal --31 rows affected
+    SET latitud_ = latitud;
+
+ALTER TABLE sucursal
+DROP COLUMN latitud;
+
+ALTER TABLE sucursal
+RENAME COLUMN latitud_ TO latitud;
+        
+-- Fill missing values in categorical columns
+UPDATE sucursal -- 0 rows updated
+SET  provincia = COALESCE(provincia, 'Sin Dato')
+WHERE provincia IS NULL;
+
+UPDATE sucursal -- 0 rows updated
+SET sucursal  = COALESCE(sucursal, 'Sin Dato')
+WHERE sucursal IS NULL;
+
+UPDATE sucursal -- 0 rows updated
+SET direccion = COALESCE(direccion, 'Sin Dato')
+WHERE direccion IS NULL;
+
+UPDATE sucursal -- 0 rows updated
+SET localidad = COALESCE(localidad, 'Sin Dato')
+WHERE localidad IS NULL;
+
+
+---------- TABLE EMPLEADO ------------
+
+SELECT * FROM empleado; 
+-- Check Sucursal, estandardize names
+
+SELECT DISTINCT sucursal
+FROM empleado
+ORDER BY 1;
+
+UPDATE empleado -- 267 rows updated
+SET sucursal =
+    CASE
+        WHEN sucursal = 'Palermo 1' THEN 'Palermo1'
+        WHEN sucursal = 'Palermo 2' THEN 'Palermo2'
+        WHEN sucursal = 'Mendoza 1' THEN 'Mendoza1'
+        WHEN sucursal = 'Mendoza 2' THEN 'Mendoza2'
+        WHEN sucursal = 'MDQ1' THEN 'Mar del Plata1'
+        WHEN sucursal = 'MDQ2' THEN 'Mar del Plata2'
+        WHEN sucursal = 'Cordoba Quiroz' THEN 'Córdoba Quiroz'
+        ELSE sucursal
+    END;
+    
+-- Check Salario, turn into decimal
+DESCRIBE TABLE empleado; -- Salario is varchar
+
+UPDATE empleado -- 267 rows updated
+    SET salario = CAST(REPLACE(salario, ',', '.') AS DECIMAL(8,2));
+
+-- Check for duplicates in ID
+SELECT IdEmpleado, count(*)
+FROM empleado
+GROUP BY IdEmpleado
+HAVING count(*) > 1; -- There are multiple id with duplicates, some have 3
+
+-- Create a new unique id combining with sucursal id
+-- Check if the join works ok
+SELECT e.*, s.IdSucursal
+FROM empleado e JOIN sucursal s
+    ON (e.sucursal = s.sucursal);
+
+-- Check if there are inconsistencies in the sucursal name between tables
+SELECT DISTINCT sucursal from empleado
+WHERE sucursal NOT IN (SELECT sucursal from sucursal); -- looks good
+
+-- Add a column with sucursal id
+ALTER TABLE empleado
+ADD IdSucursal INT NULL DEFAULT 0;
+
+UPDATE empleado e -- 267 rows updated
+    SET e.IdSucursal = s.IdSucursal
+    FROM sucursal s    
+    WHERE e.sucursal = s.sucursal;
+
+-- Create a unique id multiplying the sucursal id and adding the empleado id number
+ALTER TABLE empleado
+ADD CodigoEmpleado INT NULL DEFAULT 0;
+
+UPDATE empleado
+    SET CodigoEmpleado = IdEmpleado;
+
+UPDATE empleado
+    SET IdEmpleado = (IdSucursal*100000) + CodigoEmpleado;
+
+-- Fill mising values in categorical columns
+UPDATE empleado --  0 rows updated
+SET nombre = COALESCE(nombre, 'Sin Dato')
+WHERE nombre IS NULL;
+
+UPDATE empleado --  0 rows updated
+SET apellido = COALESCE(apellido, 'Sin Dato')
+WHERE apellido IS NULL;
+
+UPDATE empleado -- 0  rows updated
+SET sucursal  = COALESCE(sucursal, 'Sin Dato')
+WHERE sucursal IS NULL;
+
+UPDATE empleado -- 0  rows updated
+SET sector = COALESCE(sector, 'Sin Dato')
+WHERE sector IS NULL;
+
+UPDATE empleado -- 0  rows updated
+SET cargo = COALESCE(cargo, 'Sin Dato')
+WHERE cargo IS NULL;
+
+---------- TABLE PRODUCTO -------------
+    
+SELECT * FROM producto; 
+
+-- Check for duplicates and nulls in ID
+DESCRIBE TABLE producto;
+
+SELECT IdProducto, COUNT(*)
+FROM producto
+GROUP BY IdProducto
+HAVING COUNT(*) > 1; -- No duplicates in id
+
+-- Capitalize TIPOPRODUCTO
+UPDATE producto -- 291 rows updated
+    SET TipoProducto = INITCAP(TipoProducto);
+
+-- Check PRECIO, turn into decimal
+UPDATE producto -- 291 rows updated
+    SET precio = CAST(REPLACE(precio, ',', '.') AS DECIMAL(12,2));
+
+-- Fill missing values in categorical columns
+UPDATE producto -- 0 rows affected
+SET producto = COALESCE(producto, 'Sin Dato')
+WHERE producto IS NULL;
+
+UPDATE producto -- 12 rows affected
+SET TipoProducto = COALESCE(TipoProducto, 'Sin Dato')
+WHERE TipoProducto IS NULL;
+
+
+-------- TABLE PROVEEDOR --------------
+
+SELECT * FROM proveedor; 
+-- Check for nulls in proveedor
+-- Capiatlize DIRECCION, CIUDAD, PROVINCIA, PAIS, LOCALIDAD
+UPDATE proveedor
+    SET direccion = INITCAP(TRIM(direccion)),
+        ciudad = INITCAP(TRIM(ciudad)),
+        provincia = INITCAP(TRIM(provincia)),
+        pais = INITCAP(TRIM(pais)),
+        localidad = INITCAP(TRIM(localidad));
+
+-- Check for mispellings, standardise locations
+UPDATE  proveedor
+    SET ciudad = 'Ciudad de Buenos Aires'
+    WHERE TRIM(ciudad) = 'Boca De Atencion Monte Castro';
+
+UPDATE proveedor
+    SET ciudad = 'Córdoba'
+    WHERE TRIM(ciudad) = 'Cordoba';
+
+UPDATE proveedor
+    SET provincia = 'CABA'
+    WHERE provincia = 'Caba';
+
+UPDATE proveedor
+    SET provincia = 'Córdoba'
+    WHERE provincia = 'Cordoba';
+
+-- Rename ciudad for localidad and drop column localidad
+ALTER TABLE proveedor
+DROP COLUMN localidad;
+
+ALTER TABLE proveedor
+RENAME COLUMN ciudad TO localidad;
+
+-- Fill missing values in categorical columns
+UPDATE proveedor -- 2 rows updated
+SET proveedor = COALESCE(proveedor, 'Sin Dato')
+WHERE proveedor IS NULL;
+
+UPDATE proveedor -- 0 rows updated
+SET direccion = COALESCE(direccion, 'Sin Dato')
+WHERE direccion IS NULL;
+
+UPDATE proveedor -- 0 rows updated
+SET provincia = COALESCE(provincia, 'Sin Dato')
+WHERE provincia IS NULL;
+
+UPDATE proveedor -- 0 rows updated
+SET pais = COALESCE(pais, 'Sin Dato')
+WHERE pais IS NULL;
+
+UPDATE proveedor -- 0 rows updated
+SET localidad = COALESCE(localidad, 'Sin Dato')
+WHERE localidad IS NULL;
+
+
+------- TABLE TIPO_GASTO -----------
+
+SELECT * FROM tipo_gasto; -- Check MONTO, what is it supposed to mean in this table?
+
+--------- TABLE COMPRA ------------
+
+SELECT * FROM compra; -- Looks good, check for data types and duplicates
+DESCRIBE TABLE compra;
+
+SELECT IdCompra, COUNT(*)
+FROM compra
+GROUP BY IdCompra
+HAVING COUNT(*)>1; -- no duplicates in id
+
+--------- TABLE GASTO ---------------
+
+SELECT * FROM gasto; -- Looks good, check for data types and duplicates
+DESCRIBE TABLE gasto;
+
+SELECT IdGasto, COUNT(*)
+FROM gasto
+GROUP BY IdGasto
+HAVING COUNT(*)>1; -- no duplicates in id
+
+---------- TABLE VENTA --------------
+
+SELECT * FROM venta;
+DESCRIBE TABLE venta;
+
+-- Update the IdEmpleado column for the unique id created before in table Empleado
+UPDATE venta v -- 46645 rows updated
+SET v.IdEmpleado = e.IdEmpleado
+FROM empleado e
+WHERE v.IdEmpleado = e.CodigoEmpleado;
+
+------- CALENDAR TABLE --------------
+
+CREATE OR REPLACE TABLE TablaCalendario AS
+  SELECT 
+    DATEADD(DAY, seq4(), '2015-01-01')::DATE AS Fecha,
+    DATE_PART(YEAR, DATEADD(DAY, seq4(), '2015-01-01')) AS Anio,
+    DATE_PART(QUARTER, DATEADD(DAY, seq4(), '2015-01-01')) AS Trimestre,
+    DATE_PART(MONTH, DATEADD(DAY, seq4(), '2015-01-01')) AS Mes,
+    DATE_PART(DAYOFWEEK, DATEADD(DAY, seq4(), '2015-01-01')) AS DiaSemana,
+    DATE_PART(DAY, DATEADD(DAY, seq4(), '2015-01-01')) AS DiaMes
+  FROM TABLE(GENERATOR(ROWCOUNT => 3000));
+
 -- Verificar la tabla calendario
 SELECT * FROM TablaCalendario order by fecha desc limit 4;
 
+-- Normalize dimension tables
 
--- Tabla Canal de Venta
--- Sin transformaciones por el momento
+-- Table empleado: Make dimensions for sector, cargo, drop column sucursal
 
--- Tabla Clientes
--- Cambiar , por . en la columna 'x' y 'y'
-UPDATE cliente
-SET X = TO_DECIMAL(REPLACE(X, ',', '.'),12,10);
+CREATE OR REPLACE TABLE cargo(
+IdCargo INT AUTOINCREMENT PRIMARY KEY,
+Cargo VARCHAR(50) NOT NULL
+);
 
-UPDATE cliente
-SET Y = TO_DECIMAL(REPLACE(Y, ',', '.'),12,10);
+INSERT INTO cargo(Cargo) -- 5 rows inserted
+SELECT DISTINCT TRIM(Cargo) -- There are lead or trail spaces in this field in table Empleado
+FROM empleado
+ORDER BY 1;
+
+ALTER TABLE empleado 
+ADD IdCargo INT NOT NULL DEFAULT 0; 
+
+UPDATE empleado e -- 267 rows updated
+SET e.IdCargo = c.IdCargo
+FROM cargo c
+WHERE TRIM(e.Cargo) = c.Cargo;
 
 
--- Tabla Compra 
--- Sin transformaciones por el momento
+CREATE OR REPLACE TABLE sector(
+IdSector INT AUTOINCREMENT PRIMARY KEY,
+Sector VARCHAR(50) NOT NULL
+);
 
--- Empleado
+INSERT INTO sector(Sector) -- 6 rows inserted
+SELECT DISTINCT TRIM(Sector)
+FROM empleado
+ORDER BY 1;
+
+ALTER TABLE empleado 
+ADD IdSector INT NOT NULL DEFAULT 0;
+
+UPDATE empleado e --  267 rows updated
+SET e.IdSector = s.IdSector
+FROM sector s
+WHERE TRIM(e.Sector) = s.Sector;
+
+ALTER TABLE empleado
+DROP COLUMN cargo, sector, sucursal;
+
+
+-- Table producto: Make dimension for TipoProducto
+
+CREATE OR REPLACE TABLE tipo_producto(
+IdTipoProducto INT AUTOINCREMENT PRIMARY KEY,
+TipoProducto VARCHAR(50) NOT NULL
+);
+
+INSERT INTO tipo_producto(TipoProducto) -- 11 rows inserted
+SELECT DISTINCT TRIM(TipoProducto)
+FROM producto
+ORDER BY 1;
+
+ALTER TABLE producto
+ADD IdTipoProducto INT NOT NULL DEFAULT 0;
+
+UPDATE producto p -- 291 rows updated
+SET p.IdTipoProducto = tp.IdTipoProducto
+FROM tipo_producto tp
+WHERE TRIM(p.TipoProducto) = tp.TipoProducto;
+
+ALTER TABLE producto
+DROP COLUMN TipoProducto;
+
+-- Table proveedor: Check if possible to make dimensions for provincia, localidad, pais
+-- Table sucursal: Check if possible to make dimensions for provincia and localidad
+
+CREATE OR REPLACE TABLE localidad(
+IdLocalidad INT AUTOINCREMENT PRIMARY KEY,
+Localidad VARCHAR(250) NOT NULL,
+Provincia VARCHAR(250) NOT NULL,
+IdProvincia INT NOT NULL DEFAULT 0
+);
+
+INSERT INTO localidad(Localidad, Provincia) -- 594 rows inserted 
+SELECT DISTINCT Localidad, Provincia
+FROM cliente
+UNION 
+SELECT DISTINCT Localidad, Provincia
+FROM proveedor
+UNION
+SELECT DISTINCT Localidad,Provincia
+FROM sucursal
+ORDER BY 2,1;
+
+SELECT * FROM localidad;
+
+--Check for duplicates
+SELECT Localidad, COUNT(*)
+FROM localidad
+GROUP BY Localidad
+HAVING COUNT(*)>1; -- There are 13 localidades with two provincias, may be real according to Argentine geography
+
+SELECT * 
+FROM localidad
+WHERE Localidad = 'Ciudad de Buenos Aires'; -- delete idlocalidad 69 and 341 because they have C. Buenos Aires as provincia
+
+SELECT * 
+FROM localidad
+WHERE Localidad = 'Sin Dato'; -- delete idlocalidad 584 as it is Sin dato - Tucuman
+
+DELETE FROM localidad -- 3 rows deleted
+WHERE IdLocalidad = 69 OR IdLocalidad = 584 OR IdLocalidad = 341;
+
+SELECT DISTINCT Provincia
+FROM localidad;-- Correct mispelling
+
+UPDATE localidad
+    SET provincia =
+        CASE
+            WHEN provincia = 'Rio Negro' THEN 'Río Negro'
+            WHEN provincia = 'Tucuman' THEN 'Tucumán'
+            ELSE provincia
+        END;
+
+
+CREATE OR REPLACE TABLE provincia(
+IdProvincia INT AUTOINCREMENT PRIMARY KEY,
+Provincia VARCHAR(50) NOT NULL
+);
+
+INSERT INTO provincia(Provincia) -- 10 rows inserted
+SELECT DISTINCT Provincia
+FROM localidad
+ORDER BY 1;
+
+SELECT * FROM provincia;
+
+UPDATE localidad l -- 591 rows updated
+SET l.IdProvincia = p.IdProvincia
+FROM provincia p
+WHERE l.provincia = p.provincia;
+
+ALTER TABLE localidad
+DROP COLUMN Provincia;
+
+-- Add columns for IdLocalidad  and drop old columns of localidad in cliente, sucursal, proveedor
+ALTER TABLE cliente
+ADD IdLocalidad INT NOT NULL DEFAULT 0;
+
+UPDATE cliente c -- 3407 rows updated
+SET c.IdLocalidad = l.IdLocalidad
+FROM localidad l
+WHERE c.Localidad = l.Localidad;
+
+ALTER TABLE cliente
+DROP COLUMN Provincia, Localidad;
+
+ALTER TABLE sucursal
+ADD IdLocalidad INT NOT NULL DEFAULT 0;
+
+UPDATE sucursal s -- 31 rows updated
+SET s.IdLocalidad = l.IdLocalidad
+FROM localidad l
+WHERE s.Localidad = l.Localidad;
+
+ALTER TABLE sucursal
+DROP COLUMN Provincia, Localidad;
+
+ALTER TABLE proveedor
+ADD IdLocalidad INT NOT NULL DEFAULT 0;
+
+UPDATE proveedor p -- 14 rows updated
+SET p.IdLocalidad = l.IdLocalidad
+FROM localidad l
+WHERE p.Localidad = l.Localidad;
+
+ALTER TABLE proveedor
+DROP COLUMN Provincia, Localidad, Pais;
+
+
+-- Create primary and foreign keys to fact and dim tables
--- loading_tables.sql
+++ loading_tables.sql
@@ -1,239 +1,206 @@
--- CREATE DATABASE AND WAREHOUSE
-CREATE OR REPLACE DATABASE DE-SNOWFLAKE-AWS;
+-- Create database and warehouse
+CREATE OR REPLACE DATABASE RETAIL_SNOWFLAKE_AWS;
 CREATE OR REPLACE WAREHOUSE RETAIL_WH;
 
-USE DE_SNOWFLAKE_AWS;
+USE DATABASE RETAIL_SNOWFLAKE_AWS;
 
--- Linking AWS to Snowflake
-CREATE OR REPLACE STAGE s3_stage
-URL = 's3://de-snowflake-raw/'
-CREDENTIALS = (AWS_KEY_ID = '' AWS_SECRET_KEY = '');
-
-LIST @s3_stage;
-
--- Table tipo_gasto
-
-CREATE  or replace table tipo_gasto(
-Id_Gasto int,
-descripcion varchar(50),
-Monto int 
+-- Create the stage from AWS s3 bucket
+CREATE OR REPLACE STAGE retail_s3_stage
+URL = 's3://de-adventurework-snowflake-raw/data'
+CREDENTIALS = (
+  AWS_KEY_ID = ''
+  AWS_SECRET_KEY = ''
 );
 
-COPY INTO tipo_gasto
-FROM @s3_stage/TiposDeGasto.csv
-FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1);
+-- Check the connection
+LIST @retail_s3_stage;
 
-SELECT * FROM tipo_gasto;
+-- Create the tables 
+CREATE OR REPLACE TABLE tipo_gasto(
+IdTipoGasto int,
+Descripcion varchar(50),
+Monto int
+);
 
+CREATE OR REPLACE TABLE sucursal(
+IdSucursal int,
+Sucursal varchar(50),
+Direccion varchar(250),
+Localidad varchar(250),
+Provincia varchar(250),
+Latitud varchar(250),
+Longitud varchar(250)
+);
 
--- Table venta ------------------
+CREATE OR REPLACE TABLE proveedor(
+IdProveedor int,
+Proveedor varchar(50),
+Direccion varchar(250),
+Ciudad varchar(250),
+Provincia varchar(250),
+Pais varchar(50),
+Localidad varchar(250)
+);
 
-CREATE  or replace table venta(
-Id_Venta int,
-fecha date,
-fecha_entrega date,
-id_Canal int,
-Id_Cliente int,
-Id_Sucursal int,
-Id_Empleado int,
-Id_Producto int,
+CREATE OR REPLACE TABLE canal_venta(
+IdCanal int, 
+Canal varchar(50)
+);
+
+CREATE OR REPLACE TABLE cliente(
+IdCliente int,
+Provincia varchar(250),
+NombreApellido varchar(250),
+Direccion varchar(250),
+Telefono varchar(50),
+Edad varchar(50),
+Localidad varchar(250),
+Longitud varchar(50),
+Latitud varchar(50),
+Fecha_Alta varchar(50),
+Usuario_Alta varchar(50),
+Fecha_Ultima_Modificacion varchar(50),
+Usuario_Ultima_Modificacion varchar(50),
+Marca_Baja varchar(50),
+col10 int
+);
+
+CREATE OR REPLACE TABLE empleado(
+IdEmpleado int,
+Apellido varchar(50),
+Nombre varchar(50),
+Sucursal varchar(50),
+Sector varchar(50),
+Cargo varchar(50),
+Salario varchar(50)
+);
+
+CREATE OR REPLACE TABLE producto(
+IdProducto int,
+Producto varchar(250),
+TipoProducto varchar(50),
+Precio varchar(50)
+);
+
+CREATE OR REPLACE TABLE venta(
+IdVenta int,
+Fecha date,
+Fecha_Entrega date,
+IdCanal int,
+IdCliente int,
+IdSucursal int,
+IdEmpleado int,
+IdProducto int,
 Precio decimal(10,2),
 Cantidad int
 );
 
-COPY INTO venta
-FROM @s3_stage/Venta.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_DELIMITER = ',' SKIP_HEADER = 1);
-
-SELECT * FROM venta limit 10;
-
-
--- Table canal ------------------
-
-CREATE  or replace table canal(
-id_canal int,
-canal_nombre varchar(50)
+CREATE OR REPLACE TABLE compra(
+IdCompra int,
+Fecha date,
+IdProducto int,
+Cantidad int,
+Precio decimal(10,2),
+IdProveedor int
 );
 
-
-COPY INTO canal
-FROM @s3_stage/CanalDeVenta.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_DELIMITER = ',' SKIP_HEADER = 1);
-
-
-SELECT * FROM canal limit 10;
-
--- Table Clientes ------------------
--- columns: ID	Provincia	Nombre_y_Apellido	Domicilio	Telefono	Edad	Localidad	X	Y	Fecha_Alta	Usuario_Alta	Fecha_Ultima_Modificacion	Usuario_Ultima_Modificacion	Marca_Baja	col10
-
-
-CREATE  or replace table clientes(
-id_cliente int,
-provincia varchar(200),  
-nombre_y_apellido varchar(200),
-domicilio varchar(200),
-telefono varchar(50),
-edad int,
-localidad varchar(50),
-x VARCHAR(20),
-y VARCHAR(20),
-fecha_alta date,
-usuario_alta varchar(50),
-fecha_ultima_modificacion date,
-usuario_ultima_modificacion varchar(50),
-marca_baja varchar(50),
-col10 varchar(50)
+CREATE OR REPLACE TABLE gasto(
+IdGasto int,
+IdSucursal int,
+IdTipoGasto int,
+Fecha date,
+Monto decimal(10,2)
 );
 
-COPY INTO clientes
-FROM @s3_stage/Clientes.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_DELIMITER = ';' SKIP_HEADER = 1);
+-- Copy files from bucket into tables
+COPY INTO tipo_gasto
+FROM @retail_s3_stage/TiposDeGasto.csv
+FILE_FORMAT = (
+    TYPE = CSV 
+    SKIP_HEADER = 1); -- Skip the first row (header) in the CSV file.
 
-SELECT * FROM clientes limit 10;
-
--- Table Compra ------------------
--- Columns: IdCompra	Fecha	IdProducto	Cantidad	Precio	IdProveedor
-
-CREATE  or replace table compra(
-id_compra int,
-fecha date,
-id_producto int,
-cantidad int,
-precio decimal(10,2),
-id_proveedor int
-);
-
-COPY INTO compra
-FROM @s3_stage/Compra.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_DELIMITER = ',' SKIP_HEADER = 1);
-
-SELECT * FROM compra limit 10;
-
--- Table Empleados ------------------
--- Columns: ID_empleado	Apellido	Nombre	Sucursal	Sector	Cargo	Salario
-
-CREATE  or replace table empleados(
-id_empleado int,
-apellido varchar(50),
-nombre varchar(50),
-sucursal varchar(50),
-sector varchar(50),
-cargo varchar(50),
-salario decimal(10,2)
-);
-
-COPY INTO empleado
-FROM @s3_stage/Empleados.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_DELIMITER = ',' SKIP_HEADER = 1);
-
-SELECT * FROM empleados limit 10;
-
--- Table Gasto ------------------
--- Columns: IdGasto	IdSucursal	IdTipoGasto	Fecha	Monto
-
-CREATE  or replace table gasto(
-id_gasto int,
-id_sucursal int,
-id_tipo_gasto int,
-fecha date,
-monto decimal(10,2)
-);
-
-COPY INTO gasto
-FROM @s3_stage/Gasto.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_DELIMITER = ',' SKIP_HEADER = 1);
-
-SELECT * FROM gasto limit 10;
-
-
--- Table Producto ------------------
--- Columns: ID	ID_PRODUCTO	Concepto	Tipo	 Precio 
-
-CREATE  or replace table producto(
-id_producto int,
-producto varchar(50),
-tipo varchar(50),
-precio decimal(10,2)
-);
-
-COPY INTO producto
-FROM @s3_stage/Producto.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_DELIMITER = ',' SKIP_HEADER = 1);
-
-SELECT * FROM producto limit 10;
-
--- Table Proveedor ------------------
--- Columns: IDProveedor	Nombre	Address	City	State	Country	departamen
-
-CREATE  or replace table proveedor(
-id_proveedor int,
-nombre varchar(50),
-direccion varchar(50),
-ciudad varchar(50),
-estado varchar(50),
-pais varchar(50),
-departamento varchar(50)
-);
-
-COPY INTO proveedor
-FROM @s3_stage/Proveedores.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_DELIMITER = ',' SKIP_HEADER = 1);
-
-SELECT * FROM proveedor limit 10;
-
-
--- Table Sucursal ------------------
--- Columns: ID	Sucursal	Direccion	Localidad	Provincia	Latitud	Longitud
-
-CREATE  or replace table sucursal(
-id_sucursal int,
-sucursal varchar(50),
-direccion varchar(50),
-localidad varchar(50),
-provincia varchar(50),
-latitud varchar(50),
-longitud varchar(50)
-);
+SELECT * FROM tipo_gasto;
 
 COPY INTO sucursal
-FROM @s3_stage/Sucursales.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_DELIMITER = ';' SKIP_HEADER = 1);
+FROM @retail_s3_stage/Sucursales.csv
+FILE_FORMAT = (
+    TYPE = CSV 
+    FIELD_DELIMITER = ';'
+    SKIP_HEADER = 1);
 
-SELECT * FROM sucursal limit 10;
+SELECT * FROM sucursal;
+
+COPY INTO proveedor
+FROM @retail_s3_stage/Proveedores.csv
+FILE_FORMAT = (
+    TYPE = CSV 
+    FIELD_DELIMITER = ';'
+    SKIP_HEADER = 1);
+
+SELECT * FROM proveedor;
+
+COPY INTO canal_venta
+FROM @retail_s3_stage/Canalventa.csv
+FILE_FORMAT = (
+    TYPE = CSV 
+    FIELD_DELIMITER = ';'
+    SKIP_HEADER = 1
+    ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE);
+
+SELECT * FROM canal_venta;
 
 
--- Table Venta ------------------
--- Columns: IdVenta	Fecha	Fecha_Entrega	IdCanal	IdCliente	IdSucursal	IdEmpleado	IdProducto	Precio	Cantidad
+COPY INTO cliente
+FROM @retail_s3_stage/Clientes.csv
+FILE_FORMAT = (
+    TYPE = CSV 
+    FIELD_DELIMITER = ';'
+    SKIP_HEADER = 1);
 
-CREATE  or replace table venta(
-id_venta int,
-fecha date,
-fecha_entrega date,
-id_canal int,
-id_cliente int,
-id_sucursal int,
-id_empleado int,
-id_producto int,
-precio decimal(10,2),
-cantidad int
-);
+SELECT * FROM cliente;
+
+COPY INTO empleado
+FROM @retail_s3_stage/Empleados.csv
+FILE_FORMAT = (
+    TYPE = CSV
+    FIELD_DELIMITER = ';'
+    SKIP_HEADER = 1);
+
+SELECT * FROM empleado;
+
+COPY INTO producto
+FROM @retail_s3_stage/Productos.csv
+FILE_FORMAT = (
+    TYPE = CSV
+    FIELD_DELIMITER = ';'
+    SKIP_HEADER = 1
+    ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE);
+
+SELECT * FROM producto;
 
 COPY INTO venta
-FROM @s3_stage/Venta.csv
-FILE_FORMAT = (TYPE = 'CSV' FIELD_DELIMITER = ',' SKIP_HEADER = 1);
+FROM @retail_s3_stage/Venta.csv
+FILE_FORMAT = (
+    TYPE = CSV
+    FIELD_DELIMITER = ','
+    SKIP_HEADER = 1);
 
-SELECT * FROM venta limit 10;
+SELECT * FROM venta;
 
+COPY INTO compra
+FROM @retail_s3_stage/Compra.csv
+FILE_FORMAT =(
+    TYPE = CSV
+    FIELD_DELIMITER = ','
+    SKIP_HEADER = 1);
 
+SELECT * FROM compra;
 
+COPY INTO gasto
+FROM @retail_s3_stage/Gasto.csv
+FILE_FORMAT = (
+    TYPE = CSV
+    FIELD_DELIMITER = ','
+    SKIP_HEADER = 1);
 
-
-
-
-
-
-
-
-
-
-
+SELECT * FROM gasto;
